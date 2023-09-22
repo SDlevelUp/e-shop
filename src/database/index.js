00e10672c85815710f8debf5e93d69b76ba7531b
Sarah Berri <sarahberri@outlook.fr>
2023-09-22 17:54:41 +0200

    maj

--- src/database/index.js
+++ src/database/index.js
@@ -6,17 +6,16 @@ const configOptions = {
 };
 
 const connectToDB = async () => {
-    const connectionUrl = (process.env.MONGODB_URL);
-    console.log("Valeur de MONGODB_URL :", process.env.MONGODB_URL); 
+    const connectionUrl = (process.env.MONGODB_URI);
+    console.log("Valeur de MONGODB_URL :", process.env.MONGODB_URI);
     console.log("Tentative de connexion à la base de données...");
 
     try {
         await mongoose.connect(connectionUrl, configOptions);
         console.log("Connexion à la base de données réussie !");
-       
+
     } catch (error) {
         console.error("Erreur lors de la connexion à la base de données :", error.message);
-       
     }
 };
 

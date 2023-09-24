3a9f1256fff451cca0a367c61271f13f1f2131c7
Sarah Berri <sarahberri@outlook.fr>
2023-09-24 21:47:36 +0200

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,15 @@
+'use client';
+import { useContext } from "react";
+import { GlobalContext } from "@/context";
+
 export default function Home() {
+  const { isAuthUser } = useContext(GlobalContext)
+  console.log(isAuthUser);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-       
+      <div className="text-5xl text-blue-100">
+        homepage e-commerce website
       </div>
     </main>
   );
--- src/components/FormElements/InputComponent/InputComponent.js
+++ src/components/FormElements/InputComponent/InputComponent.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import VisibilityOffOutlinedIcon from '@mui/icons-material/VisibilityOffOutlined';
 import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
-import EmailIcon from '@mui/icons-material/Email';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
 
 export default function InputComponent({
     label,
@@ -20,16 +21,37 @@ export default function InputComponent({
             <p className="pt-0 pr-2 pb-0 pl-2 absolute -mt-3 mr-0 mb-0 ml-2 font-medium text-sm text-gray-600 bg-white">
                 {label}
             </p>
-            <div className="flex items-center">
-                {type === "email" && <EmailIcon />}
+            <div className="absolute top-1/2 right-4 transform -translate-y-1/2 text-gray-600">
+                {type === "email" && (
+                    <EmailOutlinedIcon />
+                )}
+
+            </div>
+            <div className="absolute top-1/2 right-4 transform -translate-y-1/2 text-gray-600">
+                {type === "text" && (
+                    <PersonOutlineOutlinedIcon />
+                )}
+
             </div>
             <input
                 placeholder={placeholder}
                 type={inputType}
                 value={value}
                 onChange={onChange}
-                className={`border text-black placeholder-gray-400 focus:outline-none focus:border-black w-full pt-4 pr-4 pb-4 pl-4 mr-0 mt-0 ml-0 text-base block bg-white border-gray-300 rounded-md ${type === "password" ? "pr-12" : "" // Ajoutez cette classe pour décaler l'icône à l'intérieur du champ de mot de passe
-
+                className={`
+                    border 
+                    text-black 
+                    placeholder-gray-400 
+                    focus:outline-none 
+                    focus:border-black 
+                    w-full 
+                    p-4
+                    m-0
+                    text-base 
+                    block 
+                    bg-white 
+                    border-gray-300 
+                    rounded-md ${type === "password" ? "pr-12" : ""
 
                     }`}
 

aba03cdb90f63f61aec57367ec768dbb0a618aac
Sarah Berri <sarahberri@outlook.fr>
2023-10-14 05:52:08 +0200

    maj

--- src/components/Navbar/Dropdown.js
+++ src/components/Navbar/Dropdown.js
@@ -1,33 +1,23 @@
 'use client'
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+
+
+import { useState } from "react";
 
 export default function Dropdown({ trigger, submenu, isOpen, onClose }) {
-    const router = useRouter();
+    const [modalOpen, setModalOpen] = useState(false);
 
     const handleSubMenuClick = (path) => {
         onClose();
-        router.push(path);
+        
+        setModalOpen(true);
     };
 
-    useEffect(() => {
-        const handleRouteChange = () => {
-            onClose();
-        };
-
-        router.events.on("routeChangeStart", handleRouteChange);
-
-        return () => {
-            router.events.off("routeChangeStart", handleRouteChange);
-        };
-    }, [router, onClose]);
-
     return (
-        <div className={`relative ${isOpen ? 'block' : 'hidden'}`}>
+        <div className={`relative ${isOpen ? "block" : "hidden"}`}>
             {trigger}
             {isOpen && submenu && Array.isArray(submenu) && submenu.length > 0 && (
-                <div className="absolute top-full left-0 w-48 bg-white border border-gray-300 rounded-lg shadow-lg z-10">
-                    <ul className="pl-2 pt-1 space-y-1">
+                <div className="absolute top-0 left-0 w-52 bg-white border border-gray-300 rounded-lg shadow-lg z-10">
+                    <ul className="pl-4 pt-1 space-y-1">
                         {submenu.map((item) => (
                             <li
                                 key={item.id}
@@ -40,6 +30,7 @@ export default function Dropdown({ trigger, submenu, isOpen, onClose }) {
                     </ul>
                 </div>
             )}
+           
         </div>
     );
 }
